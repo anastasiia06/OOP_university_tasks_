6508531bd6efa7d6794d7aada228f76f36d9bac8
anastasiia06 <anastasiia.lily06@gmail.com>
2025-04-06 13:37:24 +0300

    Add files via upload

---
+++ lab3(1).cs
@@ -0,0 +1,63 @@
+﻿class Ball
+{
+    private double r; //radius
+    private double t; //thickness
+
+
+    public Ball(double r, double t)
+    {
+        this.r = r;
+        this.t = t;
+    }
+
+    public double Area() //площа
+    {
+        return 4 * Math.PI * Math.Pow(r, 2);
+    }
+
+    public double Volume() // об'єм
+    {
+        return (4.0 / 3) * Math.PI * Math.Pow(r, 3);
+    }
+
+    public double Mass() // маса
+    {
+        return Volume() * t;
+    }
+
+    public double CrossSection() // обчислення площі перерізу
+    {
+        return Math.PI * Math.Pow(r, 2);
+    }
+
+    public void Information()
+    {
+        Console.WriteLine("Характеристики кулі:");
+        Console.WriteLine($"Радіус: {r}");
+        Console.WriteLine($"Щільність: {t}");
+        Console.WriteLine($"Площа поверхні: {Area():F2}");
+        Console.WriteLine($"Об'єм: {Volume():F2}");
+        Console.WriteLine($"Маса: {Mass():F2}");
+        Console.WriteLine($"Площа центрального перерізу: {CrossSection():F2}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.Write("Введіть радіус кулі: ");
+        double r = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введіть щільність кулі: ");
+        double t = Convert.ToDouble(Console.ReadLine());
+
+
+        Ball ball = new Ball(r, t);
+
+
+        ball.Information();
+    }
+}
+
---
+++ lab3(2).cs
@@ -0,0 +1,38 @@
+﻿
+class Coordinate
+{
+    private double x1;
+    private double y1;
+
+    public Coordinate(double x1, double y1)
+    {
+        this.x1 = x1;
+        this.y1 = y1;
+    }
+
+    public string information()
+    {
+        return $"Координати: x = {x1}, y = {y1}";
+    }
+
+    public double Distance()
+    {
+        return Math.Sqrt(x1 * x1 + y1 * y1);
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.Write("Введіть координату x: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введіть координату y: ");
+        double y = Convert.ToDouble(Console.ReadLine());
+        Coordinate coordinate = new Coordinate(x, y);
+        Console.WriteLine(coordinate.information());
+        Console.WriteLine($"Відстань до початку координат: {coordinate.Distance():F2}");
+    }
+}
+
