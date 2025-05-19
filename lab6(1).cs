811be74d378916629b725a364fb63487946ace39
anastasiia06 <anastasiia.lily06@gmail.com>
2025-05-19 15:03:45 +0300

    Lab6

---
+++ lab6(1).cs
@@ -0,0 +1,32 @@
+﻿Random o = new Random();
+int N = 8;
+List<int> numbers = new List<int>();
+
+
+for (int i = 0; i < N; i++)
+{
+    numbers.Add(o.Next(1, 91));
+}
+
+
+
+for (int i = 0; i < numbers.Count; i++)
+{
+    Console.Write("{0} ", numbers[i]);
+}
+
+for (int i = numbers.Count - 1; i >= 0; i--)
+{
+    if (numbers[i] % 3 == 0 || numbers[i] % 10 == 5)
+    {
+        numbers.RemoveAt(i);
+    }
+}
+
+
+Console.WriteLine("\nПісля видалення (кратні 3 і закінчуються на 5):");
+for (int i = 0; i < numbers.Count; i++)
+{
+    Console.Write("{0} ", numbers[i]);
+}
+
---
+++ lab6(2).cs
@@ -0,0 +1,28 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Random o = new Random();
+int N = 10;
+List<int> numbers = new List<int>();
+
+
+for (int i = 0; i < N; i++)
+    numbers.Add(o.Next(5, 51));
+
+
+for (int i = 0; i < numbers.Count; i++)
+    Console.Write(numbers[i] + " ");
+
+
+for (int i = 0; i < numbers.Count; i++)
+{
+    int lastElement = numbers[i] % 10;
+    if (lastElement != 0 && numbers[i] % lastElement == 0)
+    {
+        numbers.Insert(i, -1);
+        break;
+    }
+}
+
+Console.WriteLine("\nРезультат:");
+for (int i = 0; i < numbers.Count; i++)
+    Console.Write(numbers[i] + " ");
