2b05e61ca1e7bf6d4c37b997aeeeb0c59aa1278d
anastasiia06 <anastasiia.lily06@gmail.com>
2025-05-19 15:04:45 +0300

    Lab6

---
+++ lab6(3).cs
@@ -0,0 +1,36 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Random o = new Random();
+int N = 5;
+
+List<int> list1 = new List<int>();
+List<int> list2 = new List<int>();
+List<int> list3 = new List<int>();
+
+
+for (int i = 0; i < N; i++)
+{
+    list1.Add(o.Next(1, 11));
+    list2.Add(o.Next(1, 11));
+}
+
+
+Console.WriteLine("Колекція №1:");
+for (int i = 0; i < N; i++)
+    Console.Write(list1[i] + " ");
+
+Console.WriteLine("\nКолекція №2:");
+for (int i = 0; i < N; i++)
+    Console.Write(list2[i] + " ");
+
+
+for (int i = 0; i < N; i++)
+{
+    int max = Math.Max(list1[i], list2[i]);
+    list3.Add(max);
+}
+
+
+Console.WriteLine("\nКолекція №3:");
+for (int i = 0; i < N; i++)
+    Console.Write("{0} ", list3[i]);
